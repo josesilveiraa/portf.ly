c0d46b0850bdcf0c77e6fb555acfb6b11ac45a45
José Silveira <josesilveiraa@protonmail.com>
2023-02-15 22:47:08 -0300

    chore: remove unused import

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -4,7 +4,6 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { Prisma, Project } from '@prisma/client';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { PrismaService } from 'src/database/prisma.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
