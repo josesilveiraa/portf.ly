82ffecdbed297a09204cef98bb63c349d899e600
José Silveira <josesilveiraa@protonmail.com>
2023-02-16 16:56:31 -0300

    feat: add password encryption

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,10 +1,23 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+
+    this.$use(async (params, next) => {
+      if (params.action === 'create' && params.model === 'AdminUser') {
+        const user = params.args.data;
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(user.password, salt);
+
+        user.password = hash;
+        params.args.data = user;
+      }
+      return await next(params);
+    });
   }
 
   async enableShutdownHooks(app: INestApplication) {
