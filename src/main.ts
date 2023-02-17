66df86aa74df5c8386f038c1bcd966d9a42c013c
José Silveira <josesilveiraa@protonmail.com>
2023-02-17 14:55:31 -0300

    feat(swagger): initial swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Portfolio API')
+    .setDescription('Portfolio API description')
+    .setVersion('0.1')
+    .addTag('projects')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
 
