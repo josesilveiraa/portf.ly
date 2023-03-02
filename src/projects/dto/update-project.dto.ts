93e2f2b5796115cbc8efedfdb5bcbe285ae424c2
José Silveira <josesilveiraa@protonmail.com>
2023-03-02 13:40:04 -0300

    fix(dto): implement update project dto
    I've updated it since it didn't use to appear on Swagger API description.

--- src/projects/dto/update-project.dto.ts
+++ src/projects/dto/update-project.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsUrl, Length } from 'class-validator';
 import { CreateProjectDto } from './create-project.dto';
 
-export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
+export class UpdateProjectDto extends PartialType(CreateProjectDto) {
+  @ApiProperty({ required: false })
+  @IsNotEmpty()
+  @Length(5, 100)
+  title: string;
+
+  @ApiProperty({
+    example: 'https://github.com/josesilveiraa/josesilveiraa',
+    required: false,
+  })
+  @IsUrl()
+  @IsNotEmpty()
+  repository: string;
+
+  @ApiProperty({ required: false })
+  description: string;
+
+  @ApiProperty({ required: false })
+  @IsNotEmpty()
+  readme: string;
+}
