885f4e78e16b292da35a4c30d17e8c7219b5c53b
José Silveira <josesilveiraa@protonmail.com>
2023-03-02 13:39:13 -0300

    fix(dto): implement update user dto
    I've updated it since it didn't use to appear on Swagger API description.

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsStrongPassword, Length } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsEmail()
+  @ApiProperty({ required: false })
+  email: string;
+
+  @Length(8, 255)
+  @ApiProperty({ required: false })
+  username: string;
+
+  @IsStrongPassword()
+  @ApiProperty({ required: false })
+  password: string;
+}
