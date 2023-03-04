c8615d236d3b92f71d16f4956bf9fadd7a024a91
josesilveiraa <josesilveiraa@protonmail.com>
2023-03-03 23:17:09 -0300

    feat: use fastify
    I've changed from express to fastify since express isn't maintained actively.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import helmet from 'helmet';
 import { AppModule } from './app.module';
+import helmet from '@fastify/helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.register(helmet);
 
   const config = new DocumentBuilder()
     .setTitle('Portfolio API')
@@ -20,8 +29,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  app.use(helmet());
-
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
