d1a2412f5a65e7c852389d9ce08a2d669316a79e
josesilveiraa <josesilveiraa@protonmail.com>
2023-03-10 18:11:18 -0300

    feat: global authentication on `/api/users` endpoints

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/database/prisma.service';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [
+    UsersService,
+    PrismaService,
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
