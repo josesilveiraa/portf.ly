71ef023d2f8e549515ad12a9e249136074616094
josesilveiraa <josesilveiraa@protonmail.com>
2023-03-13 20:14:29 -0300

    refactor: remove unused SSR hook

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import type { AppProps } from 'next/app'
-import { NextUIProvider, createTheme, useSSR } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 
 const darkTheme = createTheme({ type: 'dark' });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const isBrowser = useSSR();
-
   return (
     <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
