5480f269de94b18de6d8fb072f400f49c1e3a18b
josesilveiraa <josesilveiraa@protonmail.com>
2023-03-15 18:30:42 -0300

    fix(backend): hash password when seeding the database

--- packages/backend/src/database/seed.ts
+++ packages/backend/src/database/seed.ts
@@ -1,12 +1,16 @@
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function main() {
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash('admin@123', salt);
+
   await prisma.user.create({
     data: {
       email: 'admin@admin.com',
-      password: 'admin@123',
+      password,
       username: 'admin',
     },
   });
