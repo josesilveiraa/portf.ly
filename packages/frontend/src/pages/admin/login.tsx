cc21563f92dc6d14e8eb394b00eae4702fac5d52
josesilveiraa <josesilveiraa@protonmail.com>
2023-03-18 17:37:39 -0300

    feat(frontend): implement admin login page

---
+++ packages/frontend/src/pages/admin/login.tsx
@@ -0,0 +1,45 @@
+import { Container, Text, Input, Spacer, Card, Row, Checkbox, Button } from "@nextui-org/react";
+
+export default function Login() {
+  return (
+    <Container display="flex" alignItems="center" justify="center" css={{ minHeight: '100vh' }}>
+      <Card css={{ mw: '420px', p: '20px' }}>
+          <Text
+            size={24}
+            weight="bold"
+            css={{
+              as: 'center',
+              mb: '20px',
+            }}
+          >
+            Portf.ly Dashboard
+          </Text>
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Email"
+          />
+          <Spacer y={1} />
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Password"
+          />
+          <Spacer y={1} />
+          <Row justify="space-between">
+            <Checkbox>
+              <Text size={14}>Remember me</Text>
+            </Checkbox>
+          </Row>
+          <Spacer y={1} />
+          <Button>Sign in</Button>
+        </Card>
+    </Container>
+  );
+}
