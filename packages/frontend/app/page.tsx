cd1161079180eba53ba01961dbba663e7137baf6
josesilveiraa <josesilveiraa@protonmail.com>
2023-04-14 17:29:01 -0300

    feat(frontend): implement index metadata

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import ProjectList from './components/home/ProjectList';
+
+export const metadata: Metadata = {
+  title: 'Portf.ly - Projects',
+  description: 'Welcome to Portf.ly',
+};
 
 export default function Home() {
   return (
