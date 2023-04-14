f99b3d76bbd10f85f2c2c3bcafa52e7fa10bbec0
josesilveiraa <josesilveiraa@protonmail.com>
2023-04-14 17:32:19 -0300

    feat(frontend): implement `about` metadata

--- packages/frontend/app/about/page.tsx
+++ packages/frontend/app/about/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from 'next';
 import Link from 'next/link';
 import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Portf.ly - About',
+};
 
 export default function About() {
   return (
