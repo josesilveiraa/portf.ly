f96c2c55d5f1531f5e0e753c69e9bded78fb5de5
josesilveiraa <josesilveiraa@protonmail.com>
2023-04-24 15:53:34 -0300

    feat(backend): implement `reauthenticate` method

--- packages/backend/src/auth/auth.service.ts
+++ packages/backend/src/auth/auth.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
 import { UsersRepository } from '../shared/repositories/users/users-repository';
+
+type PayloadBody = {
+  access_token: string;
+  refresh_token: string;
+};
 
 @Injectable()
 export class AuthService {
@@ -39,5 +48,33 @@ export class AuthService {
       access_token: accessToken,
       refresh_token: refreshToken,
     };
+  }
+
+  async reauthenticate(body: PayloadBody) {
+    const payload: User = await this.verifyRefreshToken(body);
+
+    return this.login(payload);
+  }
+
+  async verifyRefreshToken(body: PayloadBody) {
+    const { refresh_token } = body;
+
+    if (!refresh_token) {
+      throw new NotFoundException();
+    }
+
+    const email = this.jwtService.decode(refresh_token)['email'];
+    const user = await this.usersRepository.findOneByEmail(email);
+
+    if (!user) throw new NotFoundException();
+
+    try {
+      this.jwtService.verify(refresh_token, {
+        secret: process.env.REFRESH_JWT_SECRET,
+      });
+      return user;
+    } catch (err) {
+      throw new UnauthorizedException(err.name);
+    }
   }
 }
